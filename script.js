9789a88da03c6a32fe0e8ae4f86f4a9af6335af1
Tugsbayar Galbadrakh <tugs1ga@gmail.com>
2026-02-10 14:38:24 +0100

    web-9.1-9.2-klar

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
+// Rubrik
 let rubrik = document.querySelector("h1");
-
-console.log("rubriken text är" +rubrik.textContent);
-rubrik.textContent= "Javascript it is";
-
-let p = document.querySelector("p");
-
-p.textContent= "Text has changed due to javascript";
-
-let text = document.querySelector(".text");
-
-text.style.border="4px solid black";
-
-text.style.marginLeft= "10vw";
-text.style.marginRight="10vw";
-
+rubrik.textContent = "Javascript it is";
 rubrik.classList.add("xtra");
 
+// Paragrafer
+let p = document.querySelector("p");
+p.textContent = "Text has changed due to JavaScript";
+
+let text = document.querySelector(".text");
+text.style.border = "4px solid black";
+text.style.marginLeft = "10vw";
+text.style.marginRight = "10vw";
+
+// Bakgrund
 document.body.classList.add("yellow_background");
 
+// Lista
 let listan = document.querySelector("ul");
-let li_1 = document.createElement("li");
 
-li_1.textContent="Lära javscript syntax";;
-listan.appendChild(li_1);
+let li1 = document.createElement("li");
+li1.textContent = "Lära JavaScript syntax";
+listan.appendChild(li1);
 
-let li_2 = document.createElement("li");
-li_2.textContent="Lära document objektet";
-listan.appendChild(li_2);
+let li2 = document.createElement("li");
+li2.textContent = "Lära document-objektet";
+listan.appendChild(li2);
 
 listan.classList.add("ul_layout");
-let li_array = document.querySelectorAll("li");
 
-for(let i=0; i < li_array.length; i++)
-{
-li_array[i].classList.add("fet_text");
+let li_array = document.querySelectorAll("li");
+for (let i = 0; i < li_array.length; i++) {
+    li_array[i].classList.add("fet_text");
+}
+
+// Växla tema
+function växla_tema() {
+    document.body.classList.toggle("dark_mode");
+}
+
+// Ändra text
+let ny_text = document.querySelector("#ny_text");
+
+function lägg_till() {
+    text.textContent = ny_text.value;
+    ny_text.value = "";
+}
+
+// Byt bild
+let bilden = document.querySelector(".bilden");
+
+function byt_bild(fil_sökväg) {
+    bilden.src = fil_sökväg;
 }
